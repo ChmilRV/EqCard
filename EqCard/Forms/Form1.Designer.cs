eb6a2ae5778c78e842485eee445dc292b4fbbbf2
Chmil Ruslan <chmilrv@users.noreply.github.com>
2021-09-15 21:42:49 +0300

    2142

--- EqCard/Forms/Form1.Designer.cs
+++ EqCard/Forms/Form1.Designer.cs
@@ -61,7 +61,7 @@ namespace EqCard
 			this.dataGridView_Location.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dataGridView_Location.Location = new System.Drawing.Point(18, 23);
 			this.dataGridView_Location.Name = "dataGridView_Location";
-			this.dataGridView_Location.Size = new System.Drawing.Size(287, 290);
+			this.dataGridView_Location.Size = new System.Drawing.Size(495, 290);
 			this.dataGridView_Location.TabIndex = 0;
 			// 
 			// tabControl
@@ -72,7 +72,7 @@ namespace EqCard
 			this.tabControl.Location = new System.Drawing.Point(0, 21);
 			this.tabControl.Name = "tabControl";
 			this.tabControl.SelectedIndex = 0;
-			this.tabControl.Size = new System.Drawing.Size(677, 383);
+			this.tabControl.Size = new System.Drawing.Size(1192, 424);
 			this.tabControl.TabIndex = 1;
 			// 
 			// tabPage_Equipment
@@ -82,7 +82,7 @@ namespace EqCard
 			this.tabPage_Equipment.Location = new System.Drawing.Point(4, 22);
 			this.tabPage_Equipment.Name = "tabPage_Equipment";
 			this.tabPage_Equipment.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage_Equipment.Size = new System.Drawing.Size(669, 357);
+			this.tabPage_Equipment.Size = new System.Drawing.Size(1184, 398);
 			this.tabPage_Equipment.TabIndex = 0;
 			this.tabPage_Equipment.Text = "Оборудование";
 			this.tabPage_Equipment.UseVisualStyleBackColor = true;
@@ -94,16 +94,16 @@ namespace EqCard
 			this.tabPage_Storage.Location = new System.Drawing.Point(4, 22);
 			this.tabPage_Storage.Name = "tabPage_Storage";
 			this.tabPage_Storage.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage_Storage.Size = new System.Drawing.Size(669, 357);
+			this.tabPage_Storage.Size = new System.Drawing.Size(1184, 398);
 			this.tabPage_Storage.TabIndex = 1;
 			this.tabPage_Storage.Text = "Склад";
 			this.tabPage_Storage.UseVisualStyleBackColor = true;
 			// 
 			// statusStrip1
 			// 
-			this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+			this.statusStrip1.Location = new System.Drawing.Point(0, 579);
 			this.statusStrip1.Name = "statusStrip1";
-			this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+			this.statusStrip1.Size = new System.Drawing.Size(1264, 22);
 			this.statusStrip1.TabIndex = 2;
 			this.statusStrip1.Text = "statusStrip1";
 			// 
@@ -116,7 +116,7 @@ namespace EqCard
             this.toolStripMenuItem_About});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.Size = new System.Drawing.Size(1264, 24);
 			this.menuStrip1.TabIndex = 3;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
@@ -149,23 +149,23 @@ namespace EqCard
 			this.dataGridView_Spare.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dataGridView_Spare.Location = new System.Drawing.Point(18, 16);
 			this.dataGridView_Spare.Name = "dataGridView_Spare";
-			this.dataGridView_Spare.Size = new System.Drawing.Size(269, 291);
+			this.dataGridView_Spare.Size = new System.Drawing.Size(571, 291);
 			this.dataGridView_Spare.TabIndex = 0;
 			// 
 			// dataGridView_Equipment
 			// 
 			this.dataGridView_Equipment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView_Equipment.Location = new System.Drawing.Point(326, 23);
+			this.dataGridView_Equipment.Location = new System.Drawing.Point(567, 23);
 			this.dataGridView_Equipment.Name = "dataGridView_Equipment";
-			this.dataGridView_Equipment.Size = new System.Drawing.Size(306, 290);
+			this.dataGridView_Equipment.Size = new System.Drawing.Size(573, 290);
 			this.dataGridView_Equipment.TabIndex = 1;
 			// 
 			// dataGridView_SpareCategory
 			// 
 			this.dataGridView_SpareCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView_SpareCategory.Location = new System.Drawing.Point(314, 38);
+			this.dataGridView_SpareCategory.Location = new System.Drawing.Point(634, 16);
 			this.dataGridView_SpareCategory.Name = "dataGridView_SpareCategory";
-			this.dataGridView_SpareCategory.Size = new System.Drawing.Size(314, 269);
+			this.dataGridView_SpareCategory.Size = new System.Drawing.Size(516, 269);
 			this.dataGridView_SpareCategory.TabIndex = 1;
 			// 
 			// tabPage_EqRepairRecord
@@ -173,7 +173,7 @@ namespace EqCard
 			this.tabPage_EqRepairRecord.Controls.Add(this.dataGridView_EqRepairRecord);
 			this.tabPage_EqRepairRecord.Location = new System.Drawing.Point(4, 22);
 			this.tabPage_EqRepairRecord.Name = "tabPage_EqRepairRecord";
-			this.tabPage_EqRepairRecord.Size = new System.Drawing.Size(669, 357);
+			this.tabPage_EqRepairRecord.Size = new System.Drawing.Size(1184, 398);
 			this.tabPage_EqRepairRecord.TabIndex = 2;
 			this.tabPage_EqRepairRecord.Text = "Карточка";
 			this.tabPage_EqRepairRecord.UseVisualStyleBackColor = true;
@@ -183,20 +183,23 @@ namespace EqCard
 			this.dataGridView_EqRepairRecord.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dataGridView_EqRepairRecord.Location = new System.Drawing.Point(22, 22);
 			this.dataGridView_EqRepairRecord.Name = "dataGridView_EqRepairRecord";
-			this.dataGridView_EqRepairRecord.Size = new System.Drawing.Size(596, 244);
+			this.dataGridView_EqRepairRecord.Size = new System.Drawing.Size(882, 277);
 			this.dataGridView_EqRepairRecord.TabIndex = 0;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1264, 601);
 			this.Controls.Add(this.statusStrip1);
 			this.Controls.Add(this.menuStrip1);
 			this.Controls.Add(this.tabControl);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MainMenuStrip = this.menuStrip1;
+			this.MaximizeBox = false;
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "EqCard";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView_Location)).EndInit();
 			this.tabControl.ResumeLayout(false);

f3532098c3bc8d2dcfe48330424ef7331694905c
Chmil Ruslan <68190819+ChmilRV@users.noreply.github.com>
2022-03-31 17:07:32 +0300

    Update Location.cs

--- EqCard/Models/Location.cs
+++ EqCard/Models/Location.cs
@@ -7,16 +7,12 @@ using System.Threading.Tasks;
 
 namespace EqCard.Models
 {
-	//_____________________________________Расположение оборудования
 	class Location
 	{
 		public int Id { get; set; }
 		public string LocationName { get; set; }
 		public string LocationComment { get; set; }
 		public List<Equipment> Equipment { get; set; }
-
-
-
 
 	}
 }

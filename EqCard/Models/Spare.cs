014fd7c9c2183cdde842067c32008314423a0c0a
Chmil Ruslan <68190819+ChmilRV@users.noreply.github.com>
2022-03-31 17:08:47 +0300

    Update Spare.cs

--- EqCard/Models/Spare.cs
+++ EqCard/Models/Spare.cs
@@ -6,25 +6,20 @@ using System.Threading.Tasks;
 
 namespace EqCard.Models
 {
-	//__________________________________________Запчасти
 	class Spare
 	{
 		public int Id { get; set; }
 		public string SpareName { get; set; }
 		public string SpareComment { get; set; }
-		public int NumberInStorage { get; set; }  // количество в складе
+		public int NumberInStorage { get; set; }
 		public int SpareCategoryId { get; set; }
 		public SpareCategory SpareCategories { get; set; }
 
-		//  перечень оборудования с этой запчастью
 		public List<Equipment> Equipments { get; set; }
 		public Spare()
 		{
 			Equipments = new List<Equipment>();
 		}
-
-
-
 
 	}
 }
